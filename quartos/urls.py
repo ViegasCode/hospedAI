d158e6c171deb82cd115fcb439e3e3d919301bd2
ViegasCode <viegas.coding@gmail.com>
2025-09-04 12:03:17 -0300

    fix hospedai URLS

--- quartos/urls.py
+++ quartos/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('listar_quartos/', views.listar_quartos, name='listar_quartos'),
+    path('', views.listar_quartos, name='listar_quartos'),
 path('criar/', views.criar_quarto, name='criar_quarto'),
 ]
