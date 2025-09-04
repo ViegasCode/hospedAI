7ffc2cc9e72c48d5a962f3605fcc34ff9efeaac4
ViegasCode <viegas.coding@gmail.com>
2025-09-04 12:13:24 -0300

    fix reservas URLS

--- reservas/urls.py
+++ reservas/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("nova_reserva/", views.nova_reserva, name="nova_reserva"),
-    # path("calendario/", views.calendario_quartos, name="calendario_quartos"),
+    path("calendario-ui/", views.calendario_quarto2, name="calendario_quartos"),
     path("api/reservas/", views.reservas_json, name="reservas_json"),  # nova rota
     path("calendario/", views.calendario_dashboard, name="calendario_dashboard"),
     path("calendario2/", views.calendario_quarto2, name="calendario_quarto2"),
